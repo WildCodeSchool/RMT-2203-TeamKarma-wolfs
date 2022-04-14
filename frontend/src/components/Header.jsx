93eec13173160f46a5698c0bb85fbe23c27af2fd
OulG <lou.gain@outlook.fr>
2022-04-14 12:32:14 +0200

    Création squelette header et création header.css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,52 @@
+import { NavLink } from "react-router-dom";
+import "../styles/Header.css";
+
 import Logo from "@assets/img/svg/logo.svg";
 
 export default function Header() {
+  const getActiveLinkStyle = ({ isActive }) => {
+    if (isActive) {
+      return { color: "orange" };
+    }
+    return null;
+  };
+
   return (
     <header>
       <div className="container">
-        <div id="navbar">
+        <nav id="navbar">
           <div className="logo">
             <img src={Logo} alt="Logo cocktail finder" />
-            <h1>Cocktail Finder</h1>
+            <h1>
+              <span>c</span>ocktail <br />
+              <span>f</span>inder
+            </h1>
           </div>
           <div className="menu">
             <ul>
-              <li>Home</li>
-              <li>Favorites</li>
-              <li>Random Cocktail</li>
-              <li>Shopping List</li>
-              <li>Work in progress</li>
+              <li>
+                <NavLink style={getActiveLinkStyle} to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={getActiveLinkStyle} to="/favorites">
+                  Favorites
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={getActiveLinkStyle} to="/ramdom">
+                  Ramdom Cocktail
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={getActiveLinkStyle} to="/shopping">
+                  Shopping list
+                </NavLink>
+              </li>
             </ul>
           </div>
-        </div>
+        </nav>
       </div>
     </header>
   );
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "@pages/Home";
+
+export default function Main() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Home />} />
+        <Route path="/ramdom" element={<Home />} />
+        <Route path="/shopping" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
