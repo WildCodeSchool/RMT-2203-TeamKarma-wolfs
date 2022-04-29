c78c536460fa06bc3e335b2c79adb6eae3e5d000
anthony <anthony.aubert1@orange.fr>
2022-04-29 11:39:17 +0200

    finished US6 and css

--- frontend/src/pages/Random.jsx
+++ frontend/src/pages/Random.jsx
@@ -1,30 +1,41 @@
-import "../styles/random-card.css";
-
+import "../styles/CocktailCard.css";
+import DrinkIngredients from "@components/DrinkIngredients";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 const random = () => {
   const [randomCard, setRandomCard] = useState("");
 
-  useEffect(() => {
+  const callRandom = () => {
     axios
       .get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
       .then((response) => response.data)
       .then((data) => {
         setRandomCard(data.drinks[0]);
       });
+  };
+
+  useEffect(() => {
+    callRandom();
   }, []);
 
   return (
-    <section className="cocktail-card">
+    <section className="cocktail-card random-card">
       <div className="container">
+        <button
+          type="button"
+          className="select-button"
+          onClick={() => callRandom()}
+        >
+          Show another
+        </button>
         <div className="card-box">
           <h2 className="cocktail-name">{randomCard.strDrink}</h2>
           <div className="card-top">
             <div className="ingredients">
               <h3>Ingrédients</h3>
               <ul>
-                <p>jjjjjj</p>
+                <DrinkIngredients drink={randomCard} />
               </ul>
             </div>
             <div className="card-img">
