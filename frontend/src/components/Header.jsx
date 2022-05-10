3e408b5355bcd9e19452ad21f37dcba64ddd12e9
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-05-10 09:40:37 +0200

    added some styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -21,41 +21,51 @@ export default function Header() {
       </button>
       <div className="container">
         <nav id="navbar">
-          <div className="logo">
-            <img src={Logo} alt="Logo cocktail finder" />
-            <h1>
-              <span>c</span>ocktail <br />
-              <span>f</span>inder
-            </h1>
-          </div>
-          <div className="menu">
-            <ul>
-              <li>
-                <NavLink style={getActiveLinkStyle} to="/">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink style={getActiveLinkStyle} to="/favorites">
-                  Favorites
-                </NavLink>
-              </li>
-              <li>
-                <NavLink style={getActiveLinkStyle} to="/random">
-                  Random Cocktail
-                </NavLink>
-              </li>
-              <li>
-                <NavLink style={getActiveLinkStyle} to="/cocktails">
-                  Cocktails
-                </NavLink>
-              </li>
-              <li>
-                <NavLink style={getActiveLinkStyle} to="/shopping">
-                  Shopping list
-                </NavLink>
-              </li>
-            </ul>
+          <div className="navbar-burger">
+            <div className="containers nav-container">
+              <input className="checkbox" type="checkbox" name="" id="" />
+              <div className="hamburger-lines-mobile">
+                <span className="line line1" />
+                <span className="line line2" />
+                <span className="line line3" />
+              </div>
+              <div className="logo">
+                <img src={Logo} alt="Logo cocktail finder" />
+                <h1>
+                  <span>c</span>ocktail <br />
+                  <span>f</span>inder
+                </h1>
+              </div>
+              <div className="menu">
+                <ul>
+                  <li>
+                    <NavLink style={getActiveLinkStyle} to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink style={getActiveLinkStyle} to="/favorites">
+                      Favorites
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink style={getActiveLinkStyle} to="/random">
+                      Random Cocktail
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink style={getActiveLinkStyle} to="/cocktails">
+                      Cocktails
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink style={getActiveLinkStyle} to="/shopping">
+                      Shopping list
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </nav>
       </div>
